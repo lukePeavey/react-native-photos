23970e03a2cb3d0f96f496055d4ca4db00a91c19
Luke Peavey <1williampeavey@gmail.com>
2019-02-20 23:47:59 -0500

    Setup ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  parser: 'babel-eslint',
+  env: {
+    jest: true
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {
+        extensions: ['.js', '.android.js', '.ios.js'],
+        alias: {
+          '@data': './data',
+          '@assets': './assets',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@store': './src/store',
+        }
+      }
+    }
+  },
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/default-props-match-prop-types': 'off',
+    'react/require-default-props': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/prefer-stateless-function': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-use-before-define': 'off',
+    'operator-linebreak': 'off',
+    'no-return-assign': 'off',
+    'no-underscore-dangle': 'off',
+    'global-require': 'off',
+    'class-methods-use-this': 'off',
+    'arrow-parens': 'off',
+    'no-else-return': 'off'
+  },
+  globals: {
+    fetch: false
+  }
+}
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
+/* eslint-disable func-names */
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+  api.cache(true)
+  const plugins = [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        extensions: ['.js', '.android.js', '.ios.js'],
+        alias: {
+          '@data': './data',
+          '@assets': './assets',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@store': './src/store',
+        }
+      }
+    ]
+  ]
+  const presets = ['babel-preset-expo']
+  return { presets, plugins }
+}
