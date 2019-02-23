5a13ef737e61aea384bbfb15d8f94fda29f0b960
Luke Peavey <1williampeavey@gmail.com>
2019-02-23 03:28:31 -0500

    Support device rotation/orientation change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Dimensions } from 'react-native'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { ScreenOrientation } from 'expo'
+import { actions } from '@store'
 import AppNavigator from '@navigation/tabNavigator'
 
-export default class App extends React.Component {
+const propTypes = {
+  dispatch: PropTypes.func.isRequired,
+}
+
+class App extends React.Component {
+  static propTypes = propTypes
+
+  componentDidMount = () => {
+    ScreenOrientation.allowAsync('ALL')
+    Dimensions.addEventListener('change', this._handleOrientationChange)
+  }
+
+  componentWillUnmount = () => {
+    Dimensions.removeEventListener('change', this._handleOrientationChange)
+  }
+
+  _handleOrientationChange = ({ screen }) => {
+    const { dispatch } = this.props
+    dispatch(actions.ui.updateScreenDimensions(screen))
+  }
+
   render() {
     return <AppNavigator />
   }
 }
+
+export default connect(null)(App)
